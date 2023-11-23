55bd94bc2221124f9ee4af876f0304007a7f0987
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-11-23 22:01:22 +0200

    upp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+import React from 'react';
+import Profile from './Profile';
+
+import user from './user.json';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
+
+export default App;
