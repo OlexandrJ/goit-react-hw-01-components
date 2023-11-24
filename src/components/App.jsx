fbfeb65da4cfcdf841f2fef98d6dcda0af6a5e7a
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-11-24 10:29:54 +0200

    comm3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Profile from './Profile';
 import user from './user.json';
 import data from './data.json';
+import friends from './friends.json';
 import Statistics from './Statistics';
+import FriendList from './FriendList';
 
 export const App = () => {
   return (
@@ -16,7 +18,7 @@ export const App = () => {
       />
 
       <Statistics title="Upload stats" stats={data} />
-      <Statistics stats={data} />
+      <FriendList friends={friends} />
 
     </div>
   );
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './FriendList.css';
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
+  return (
+    <li className="item">
+      <span className={`status ${isOnline ? 'online' : 'offline'}`}></span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  );
+};
+
+FriendListItem.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
+
+const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map((friend) => (
+        <FriendListItem key={friend.id} {...friend} />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ).isRequired,
+};
+
+export default FriendList;
