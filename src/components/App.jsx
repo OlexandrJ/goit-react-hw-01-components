062493de59d39b967689b56062406f40f9ddcd3b
OlexandrJ <134488937+OlexandrJ@users.noreply.github.com>
2023-11-24 09:32:09 +0200

    comm2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,8 @@
-// export const App = () => {
-//   return (
-//     <div
-//       style={{
-//         height: '100vh',
-//         display: 'flex',
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         fontSize: 40,
-//         color: '#010101'
-//       }}
-//     >
-//       React homework template
-//     </div>
-//   );
-// };
 import React from 'react';
 import Profile from './Profile';
 import user from './user.json';
+import data from './data.json';
+import Statistics from './Statistics';
 
 export const App = () => {
   return (
@@ -28,8 +14,14 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+
     </div>
   );
 };
+
+
 
 export default App;
